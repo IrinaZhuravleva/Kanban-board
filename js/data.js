308dfacaa38ee569bdaa0cbfc2cd6a498cc5d95f
irinazhuravleva <irinazhuravleva@yandex.ru>
2022-10-07 20:31:38 +0400

    first commit

---
+++ js/data.js
@@ -0,0 +1,47 @@
+export const tasks = [
+  {
+    'id': `1`,
+    'title': `Выучить JS`,
+    'status': `processing`,
+  },
+  {
+    'id': `2`,
+    'title': `Выучить React`,
+    'status': `processing`,
+  },
+  {
+    'id': `3`,
+    'title': `Позвонить маме`,
+    'status': `done`,
+  },
+  {
+    'id': `4`,
+    'title': `Выпить смузи`,
+    'status': `processing`,
+  },
+  {
+    'id': `5`,
+    'title': `Сходить погулять`,
+    'status': `done`,
+  },
+  {
+    'id': `6`,
+    'title': `Перестать тупить`,
+    'status': `processing`,
+  },
+  {
+    'id': `7`,
+    'title': `Сделать домашку`,
+    'status': `processing`,
+  },
+  {
+    'id': `8`,
+    'title': `Прочитать Войну и Мир`,
+    'status': `done`,
+  },
+  {
+    'id': `9`,
+    'title': `Погладить Кекса`,
+    'status': `done`,
+  },
+];
